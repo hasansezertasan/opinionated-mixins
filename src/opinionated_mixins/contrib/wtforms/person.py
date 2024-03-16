a256cde7af1a50bb64c8da4e362ef452a92d1d50
hasansezertasan <hasansezertasan@gmail.com>
2024-03-16 18:53:36 +0300

    Remove incorrect type hints and add pre-commit configuration.

--- src/opinionated_mixins/contrib/wtforms/person.py
+++ src/opinionated_mixins/contrib/wtforms/person.py
@@ -1,8 +1,5 @@
-import datetime
-
-from typing_extensions import Optional
 from wtforms import DateField, EmailField, StringField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Optional
 
 
 class Person:
@@ -10,7 +7,7 @@ class Person:
     WTForms `Person` form that represents a person.
     """
 
-    first_name: str = StringField(  # type: ignore
+    first_name = StringField(
         label="First name", validators=[Length(min=1, max=64), DataRequired()]
     )
     """
@@ -18,7 +15,7 @@ class Person:
     Minimum length: 1 character.
     Maximum length: 64 characters.
     """
-    last_name: str = StringField(  # type: ignore
+    last_name = StringField(
         label="Last name", validators=[Length(min=1, max=64), DataRequired()]
     )
     """
@@ -26,68 +23,82 @@ class Person:
     Minimum length: 1 character.
     Maximum length: 64 characters.
     """
-    middle_name: Optional[str] = StringField(  # type: ignore
-        label="Middle name", validators=[Length(min=1, max=64)], default=None
+    middle_name = StringField(
+        label="Middle name",
+        validators=[Length(min=1, max=64), Optional()],
+        default=None,
     )
     """
     Middle name of the person.
     Minimum length: 1 character.
     Maximum length: 64 characters.
     """
-    phone: Optional[str] = StringField(  # type: ignore
-        label="Phone", validators=[Length(min=1, max=64)], default=None
+    phone = StringField(
+        label="Phone",
+        validators=[Length(min=1, max=64), Optional()],
+        default=None,
     )
     """
     Phone number of the person.
     Minimum length: 1 character.
     Maximum length: 64 characters.
     """
-    email: Optional[str] = EmailField(  # type: ignore
-        label="Email", validators=[Length(min=1, max=64)], default=None
+    email = EmailField(
+        label="Email",
+        validators=[Length(min=1, max=64), Optional()],
+        default=None,
     )
     """
     Email address of the person.
     Minimum length: 1 character.
     Maximum length: 64 characters.
     """
-    address: Optional[str] = StringField(  # type: ignore
-        label="Address", validators=[Length(min=1, max=64)], default=None
+    address = StringField(
+        label="Address",
+        validators=[Length(min=1, max=64), Optional()],
+        default=None,
     )
     """
     Address of the person.
     Minimum length: 1 character.
     Maximum length: 64 characters.
     """
-    postal_code: Optional[str] = StringField(  # type: ignore
-        label="Postal code", validators=[Length(min=1, max=64)]
+    postal_code = StringField(
+        label="Postal code", validators=[Length(min=1, max=64), Optional()]
     )
     """
     Postal code of the person.
     Minimum length: 1 character.
     Maximum length: 64 characters.
     """
-    city: Optional[str] = StringField(  # type: ignore
-        label="City", validators=[Length(min=1, max=64)], default=None
+    city = StringField(
+        label="City",
+        validators=[Length(min=1, max=64), Optional()],
+        default=None,
     )
     """
     City of the person.
     Minimum length: 1 character.
     Maximum length: 64 characters.
     """
-    country: Optional[str] = StringField(  # type: ignore
-        label="Country", validators=[Length(min=1, max=64)], default=None
+    country = StringField(
+        label="Country",
+        validators=[Length(min=1, max=64), Optional()],
+        default=None,
     )
     """
     Country of the person.
     Minimum length: 1 character.
     Maximum length: 64 characters.
     """
-    date_birth: Optional[datetime.date] = DateField(label="Birth date", default=None)  # type: ignore
+    date_birth = DateField(label="Birth date", validators=[Optional()], default=None)
     """
     Birth date of the person.
     """
-    description: Optional[str] = StringField(  # type: ignore
-        label="Description", validators=[Length(min=1, max=64)], default=None
+    description = StringField(
+        label="Description",
+        validators=[Length(min=1, max=64), Optional()],
+        default=None,
     )
     """
     Description of the person.
